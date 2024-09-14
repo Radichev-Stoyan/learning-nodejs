b9f8831a47945dd619b6d1d554ae918157647baf
Radichev-Stoyan <stoyan.radichev.a62@learn.telerikacademy.com>
2024-09-14 07:28:41 +0100

    Redirecting request

--- creating-server/app.js
+++ creating-server/app.js
@@ -1,12 +1,24 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
+
+  // routing request
   if (url === '/') {
     res.write('<html>');
     res.write('<head><title>Enter message</title></head>');
     res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
     res.write('</html>');
+    return res.end();
+  }
+
+  // redirecting request
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'DUMMY');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
     return res.end();
   }
   res.setHeader('Content-Type', 'text/html');
