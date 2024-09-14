9a7128da3581896b1965e34123be8b7765545d2e
Radichev-Stoyan <stoyan.radichev.a62@learn.telerikacademy.com>
2024-09-14 07:46:27 +0100

    Parsing request bodies

--- creating-server/app.js
+++ creating-server/app.js
@@ -15,12 +15,33 @@ const server = http.createServer((req, res) => {
   }
 
   // redirecting request
-  if (url === '/message' && method === 'POST') {
+  /*if (url === '/message' && method === 'POST') {
     fs.writeFileSync('message.txt', 'DUMMY');
     res.statusCode = 302;
     res.setHeader('Location', '/');
     return res.end();
+  }*/
+
+  // parsing request bodies
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+    });
+
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
   }
+
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>My first page</title></head>');
