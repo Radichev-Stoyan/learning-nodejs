240a2a33d8feee49fc371491db065fb278534939
Radichev-Stoyan <stoyan.radichev.a62@learn.telerikacademy.com>
2024-09-15 09:15:57 +0100

    Using the Node Modules System

--- creating-server/app.js
+++ creating-server/app.js
@@ -1,53 +1,7 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
+const routes = require('./routes');
 
-  // routing request
-  if (url === '/') {
-    res.write('<html>');
-    res.write('<head><title>Enter message</title></head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-    res.write('</html>');
-    return res.end();
-  }
-
-  // redirecting request
-  /*if (url === '/message' && method === 'POST') {
-    fs.writeFileSync('message.txt', 'DUMMY');
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
-  }*/
-
-  // parsing request bodies
-  if (url === '/message' && method === 'POST') {
-    const body = [];
-
-    req.on('data', (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-
-    req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
-      fs.writeFile('message.txt', message, (err) => {
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-      });
-    });
-  }
-
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>My first page</title></head>');
-  res.write('<body><h1>Hello from my Node.js server</h1></body>');
-  res.write('</html>');
-  res.end();
-});
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
