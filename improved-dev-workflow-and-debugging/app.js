4644f8f391e253abb002852cca1d57b18f07e686
Radichev-Stoyan <stoyan.radichev.a62@learn.telerikacademy.com>
2024-09-17 09:31:40 +0100

    Handling different routes

--- improved-dev-workflow-and-debugging/app.js
+++ improved-dev-workflow-and-debugging/app.js
@@ -4,12 +4,17 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('In the middleware!');
-  next(); // Allows the request to continue to the next middleware in line
+app.use('/', (req, res, next) => {
+  console.log('This always runs!');
+  next();
 });
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => { // If the path doesn't match, the callback won't be executed and it will continue to the next middleware
+  console.log('In another middleware!');
+  res.send('<h1>"Add product page"</h1>'); // Sends a response to the client so no need to call next()
+});
+
+app.use('/', (req, res, next) => {
   console.log('In another middleware!');
   res.send('<h1>Hello from Express!</h1>'); // Sends a response to the client so no need to call next()
 });
