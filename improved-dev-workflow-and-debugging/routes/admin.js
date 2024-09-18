3c27d5137cac608a33a51563b4658a9c08960a0a
Radichev-Stoyan <stoyan.radichev.a62@learn.telerikacademy.com>
2024-09-18 09:33:46 +0100

    Serving html pages

--- improved-dev-workflow-and-debugging/routes/admin.js
+++ improved-dev-workflow-and-debugging/routes/admin.js
@@ -1,9 +1,10 @@
+const path = require('path');
 const express = require('express');
 const router = express.Router();
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => { // If the path doesn't match, the callback won't be executed and it will continue to the next middleware
-  res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'); // Sends a response to the client so no need to call next()
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST
