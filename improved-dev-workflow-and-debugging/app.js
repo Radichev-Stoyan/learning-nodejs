5239a35b2148c036e6603bf9c7970099cbc9a3c3
Radichev-Stoyan <stoyan.radichev.a62@learn.telerikacademy.com>
2024-09-18 09:37:21 +0100

    Returning a 404 page

--- improved-dev-workflow-and-debugging/app.js
+++ improved-dev-workflow-and-debugging/app.js
@@ -1,5 +1,6 @@
 // const http = require('http');
 
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -14,7 +15,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not found</h1>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000); // Shortcut replacement for lines 19 and 21 and this makes line 1 unnecessary
